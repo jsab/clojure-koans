b4a1a984f1b079db2cd91ac9f8a74cc7a71a13d4
Jean-Sebastien A. Beaudry <git@jsab.me>
2013-04-25 12:15:38 +1200

    Fun stuff.

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -1,20 +1,28 @@
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (let [f (first coll)
+        n (next coll)] 
+    (if n
+      (conj (vec (recursive-reverse n)) f)
+      coll)))
 
 (defn factorial [n]
-  __)
+  (* n (loop [n (dec n)
+              acc 1]
+         (if (= 0 n)
+           acc
+           (recur (dec n) (* n acc))))))
 
 (meditations
   "Recursion ends with a base case"
